26ad0aba7317556c71f8d63999da7f5a07da3a78
Furkan <frkncesur@gmail.com>
2024-03-26 19:56:51 +0300

    Completed CdpCommandsTest

--- src/CdpCommandsTest.java
+++ src/CdpCommandsTest.java
@@ -1,8 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CdpCommandsTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		DevTools devTools = driver.getDevTools();
+		devTools.createSession();
+
+		Map deviceMetrics = new HashMap();
+		deviceMetrics.put("width", 600);
+		deviceMetrics.put("height", 1000);
+		deviceMetrics.put("deviceScaleFactor", 50);
+		deviceMetrics.put("mobile", true);
+
+		driver.executeCdpCommand("Emulation.setDeviceMetricsOverride", deviceMetrics);
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.cssSelector(".navbar-toggler")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("(//a[@class='nav-link'])[2]")).click();
 
 	}
 
